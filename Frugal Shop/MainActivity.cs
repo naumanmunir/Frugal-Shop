462b920cb57818adbf58d308f4ce63a4232c9065
nonabona <nonabona@NAUMAN>
2016-12-24 17:34:48 -0500

    Add project files.

---
+++ Frugal Shop/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V4.App;
+
+namespace Frugal_Shop
+{
+    [Activity(Label = "Frugal Shop", MainLauncher = true, Icon = "@drawable/icon", Theme ="@style/MyTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        private Android.Support.V7.Widget.Toolbar myToolBar; 
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            myToolBar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(myToolBar);
+            SupportActionBar.Title = "Frugal Shop";
+
+        }
+    }
+}
+

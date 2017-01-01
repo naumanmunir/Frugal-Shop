0405c21282b186805c1190bd244f33d4f854e12f
nonabona <bloodo9@hotmail.com>
2017-01-01 00:45:02 -0500

    third

---
+++ Frugal Shop/ContentFragment.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.App;
+
+namespace Frugal_Shop
+{
+    public class ContentFragment : Fragment
+    {
+        int position;
+
+        public static ContentFragment NewInstance(int position)
+        {
+            var frag = new ContentFragment();
+            var bundle = new Bundle();
+
+            bundle.PutInt("position", position);
+            frag.Arguments = bundle;
+            
+            return frag;
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            position = Arguments.GetInt("position");
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var root = inflater.Inflate(Resource.Layout.fragment, container, false);
+            var text = root.FindViewById<TextView>(Resource.Id.textView1);
+            text.SetTextColor(Android.Graphics.Color.Argb(255, 0, 0, 0));
+            if (position == 0)
+            {
+                text.Text = "Man";
+            }
+            else if (position == 1)
+            {
+                text.Text = "Woman";
+            }
+            else
+            {
+                text.Text = "Home";
+            }
+
+            ViewCompat.SetElevation(root, 50);
+
+            return root;
+        }
+    }
+}
--- Frugal Shop/MainActivity.cs
+++ Frugal Shop/MainActivity.cs
@@ -3,56 +3,90 @@ using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
 using Xamarin.Forms;
+using Android.Support.V4.App;
+using System;
+using Java.Lang;
+using com.refractored;
+using Android.Support.V4.View;
+using Android.Support.V7.Widget;
 
 namespace Frugal_Shop
 {
-    [Activity(Label = "Frugal Shop", MainLauncher = true, Icon = "@drawable/icon", Theme ="@style/MyTheme")]
-    public class MainActivity : AppCompatActivity 
+    [Activity(Label = "Frugal Shop", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/MyTheme")]
+    public class MainActivity : AppCompatActivity
     {
-        private Android.Support.V7.Widget.Toolbar myToolBar; 
+        private Android.Support.V7.Widget.Toolbar myToolBar;
+        MyAdapter adapt;
+        PagerSlidingTabStrip tabs;
+        ViewPager pager;
+
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
 
+
+            //adapt = new MyAdapter(SupportFragmentManager);
+            //tabs = FindViewById<PagerSlidingTabStrip>(Resource.Id.tabs);
+            //pager = FindViewById<ViewPager>(Resource.Id.pager);
+
+            //pager.Adapter = adapt;
+            //tabs.SetViewPager(pager);
+            //tabs.SetBackgroundColor(Android.Graphics.Color.Argb(255, 0, 149, 164));
+
+        }
+
+        public override void SetSupportActionBar(Android.Support.V7.Widget.Toolbar toolbar)
+        {
             myToolBar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(myToolBar);
-            SupportActionBar.Title = "Frugal Shop";
-
         }
     }
 
-    //Blue Page
-    public class BluePage : ContentPage
+
+    public class MyAdapter : FragmentPagerAdapter
     {
-        public BluePage()
+        int tabCount = 3;
+
+        public MyAdapter(Android.Support.V4.App.FragmentManager fm):base(fm)
         {
-            BackgroundColor = Color.Blue;
-            Title = "Blue Page";
-            Icon = "heart.png";
+
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return tabCount;
+            }
+        }
+
+        public override ICharSequence GetPageTitleFormatted(int position)
+        {
+            ICharSequence cs;
+
+            if (position == 0)
+            {
+                cs = new Java.Lang.String("Man");
+            }
+            else if (position == 1)
+            {
+                cs = new Java.Lang.String("Woman");
+            }
+            else
+            {
+                cs = new Java.Lang.String("Home");
+            }
+
+            return cs;
+        }
+
+        public override Android.Support.V4.App.Fragment GetItem(int position)
+        {
+            return ContentFragment.NewInstance(position);
         }
     }
 
-    // Red Page
-    public class RedPage : ContentPage
-    {
-        public RedPage()
-        {
-            BackgroundColor = Color.Red;
-            Title = "Red Page";
-            Icon = "icecream.png";
-        }
-    }
-
-    // TabPage
-    public class TabPage : TabbedPage
-    {
-        public TabPage()
-        {
-            this.Children.Add(new BluePage());
-            this.Children.Add(new RedPage());
-        }
-    }
 }
 
